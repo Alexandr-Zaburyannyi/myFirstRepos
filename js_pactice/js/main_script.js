686f38d0a1252f5b5065b78ee3780ba0e569c987
Alexandr-Zaburiannyi <ozaburiannyi@gmail.com>
2022-06-27 12:15:37 +0300

    file was modified

--- js_pactice/js/main_script.js
+++ js_pactice/js/main_script.js
@@ -1,6 +1,6 @@
 let editButtons = document.querySelectorAll('.edit');
 
-var removeButtons = document.querySelectorAll('.remove');
+let removeButtons = document.querySelectorAll('.remove');
 
 let rows = document.querySelectorAll('.table__row');
 
@@ -23,9 +23,9 @@ let rowsLength = rows.length;
 
 addButton.addEventListener('click',() => {
     container.classList.toggle('_invisible');
-    for(let i=0; i < inputs.length; i++) {
-       inputs[i].value = "";
-    }
+    inputs.forEach(input => {
+        input.value = '';
+    })
 });
 
 
@@ -33,56 +33,52 @@ addButton.addEventListener('click',() => {
 submitButton.addEventListener('click',() =>{
    
 table.insertAdjacentHTML('beforeend',
-`<tr class="table__row">
-<td class="table__row__content">${rows.length++}</td>
+`<tr class="table__row" data-id="${rowsLength}">
+<td class="table__row__content">${rowsLength}</td>
 <td class="table__row__content">${document.querySelector(".name").value}</td>
 <td class="table__row__content">${document.querySelector(".surname").value}</td>
 <td class="table__row__content">${document.querySelector(".email").value}</td>
 <td class="table__row__content">${document.querySelector('.city').value}</td>
 <td class="table__row__content">${document.querySelector('.company').value}</td>
-<td class="table__row__content"><button class="edit" >Edit</button></td>
+<td class="table__row__content"><button class="edit" data-id="${rowsLength}">Edit</button></td>
 <td class="table__row__content"><button class="remove">Remove</button></td>
 </tr>
 `);
-
+rowsLength++;
 
 container.classList.toggle('_invisible');
 })
 
 
 
-removeButtons.forEach(removeButton => {
-    removeButton.addEventListener('click',() => {
-        for(let i = 0; i < removeButtons.length; i++) {
-            if(event.target === removeButtons[i]) {
-                rows[i + 1].classList.add('_invisible');
-            } 
-        }
-    })
-})
 
-editButtons.forEach(function(editButton) {
-    
-    editButton.addEventListener('click', function(){
-        container.classList.toggle('_invisible');
-       for(let i=0; i < editButtons.length; i++) {
-        
-        if(event.target === editButtons[i]) {
-            
-            let attr = editButtons[i].getAttribute("data-id");
-            for(let j=0; j < inputs.length; j++) {
-                let temp = document.querySelector(`.table__row[data-id="${attr}"]`);
-                inputs[j].value = temp.children[j + 1].innerHTML;
+    table.addEventListener('click',() => {
+        removeButtons.forEach(button => {
+            if(event.target === button){
+                button.parentElement.parentElement.remove();
             }
+        })
+    })
 
-          
-        }
+
+
+    
+    table.addEventListener('click', function(){
+        editButtons.forEach(editButton => {
+            if (event.target === editButton){
+                let temp = editButton.parentElement.parentElement.innerText;
+                console.log(temp);
+                container.classList.toggle('_invisible');
+                inputs.forEach( input => {
+                    input = temp;
+                })
+            }
+        })
         
-       }
        
         submitButton.addEventListener('click',() =>{
        //         tableRowContainers[i + 1].innerHTML = cache;
             });
     })
-});
+
 
